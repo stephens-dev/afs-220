31c62af4d5c8598feef7aafb7274dd83513fe0c7
tekelia-powe <66314860+tekelia-powe@users.noreply.github.com>
2021-11-06 09:04:05 -0500

    Updates to NavBar and Layout
    Updated NavBar, added base layout to uniform all pages

--- Capstone/capstone/capstone/settings.py
+++ Capstone/capstone/capstone/settings.py
@@ -125,3 +125,11 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL='/static/'
+# STATIC_ROOT = '/home/stephens-pc/Documents/GitHub/afs-220/Capstone/capstone'
+# STATICFILES_DIRS = ('/home/stephens-pc/Documents/GitHub/afs-220/Capstone/capstone/static'),
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'capstone/static')
+]
--- Capstone/capstone/pages/urls.py
+++ Capstone/capstone/pages/urls.py
@@ -3,6 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('about', views.about, name='about')
+    path('', views.index, name='home'),
+    path('about', views.about, name='about'),
+    path('mountain/', views.mountain, name='mountain'),
+    path('register', views.register, name='register'),
+    path('login', views.login, name='login'),
+     path('booked', views.booked, name='booked'),
+      path('saved', views.saved, name='saved'),
 ]
